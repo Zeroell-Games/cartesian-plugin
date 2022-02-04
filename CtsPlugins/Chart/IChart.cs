f02762ed47dd36521b4a88fd11ee8d06f2e3d3b3
Lialy <chocolate_liy@163.com>
2022-02-05 02:48:04 +0800

    create project

---
+++ CtsPlugins/Chart/IChart.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cartesian.EditMode.Plugin
+{
+    public interface IChart
+    {
+        /// <summary>
+        /// 判断是否更新谱面
+        /// </summary>
+        /// <param name="version"></param>
+        /// <returns>upgraded</returns>
+        bool OnChartLoad(int version);
+        void OnNoteLoad(ICollection<ICtsEvent> events);
+        void OnTimingLoad(ICollection<ICtsEvent> events);
+
+    }
+}
---
+++ CtsPlugins/Chart/ICtsEvent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cartesian.EditMode.Plugin
+{
+    public interface ICtsEvent
+    {
+        bool GetProperty<T>(string key, out T value);
+        bool SetProperty<T>(string key, T value);
+    }
+}
